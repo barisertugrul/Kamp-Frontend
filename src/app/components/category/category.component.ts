e60c232ffb7d41e75468c69b7060dfaa7be38bd2
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2021-03-14 02:17:56 +0300

    18.Gün Ders Sonu

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,8 @@
+import { isEmptyExpression } from '@angular/compiler';
+import { isNull } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[]=[];
+  currentCategory: Category;
+  dataLoaded = false;
+  constructor(private categoryService:CategoryService) { }
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(){
+    this.categoryService.getCategories().subscribe(response=>{
+      this.categories = response.data
+      this.dataLoaded=true;
+    })
+  }
+
+  setCurrentCategory(category:Category){
+    this.currentCategory = category;
+  }
+
+  setCurrentCategoryToNull(){
+    this.currentCategory = {categoryId:0,categoryName:'All'};
+  }
+
+  getCategoryClass(category:Category){
+    if(category == this.currentCategory){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+  }
+
+  getAllCategoryClass(category:Category){
+    if(this.currentCategory == null || category.categoryName == this.currentCategory.categoryName){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
   }
 
 }
